9528c2fb2e2b7f585b565d2294b38fc9b973e9ad
Sahidev1 <theoddrogue@gmail.com>
2023-03-06 12:58:52 +0100

    production db init added

--- backend/src/util/database.js
+++ backend/src/util/database.js
@@ -1,7 +1,22 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-module.exports = new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+// Generated sequalize instance depends on if we are on production
+// or a dev runtime.
+module.exports = DATABASE_URL? 
+new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    operatorAliases: false,
+    define: {
+        timestamps: false
+    }
+}):new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST||'localhost',
     dialect: 'postgres',
     port: process.env.DB_PORT||5433,
